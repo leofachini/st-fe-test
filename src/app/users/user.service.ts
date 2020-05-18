fdab5b005dcb822215ef0ac2c3ece33292ff5253
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-18 19:11:25 +0100

    Added return type in getUserList method in UserService

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { User } from './user.model';
 
 @Injectable({
   providedIn: 'root',
@@ -9,9 +11,9 @@ export class UserService {
 
   constructor(private http: HttpClient) { }
 
-  public getUsersList(): any {
+  public getUsersList(): Observable<User[]> {
     const URL = 'https://uitest.free.beeceptor.com/usernames';
     // INFO: Operator take 1 to avoid handle unsubscribed subscriptions
-    return this.http.get(URL).pipe(take(1));
+    return <Observable<User[]>>this.http.get(URL).pipe(take(1));
   }
 }
