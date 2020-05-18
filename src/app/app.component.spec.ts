6bbc9300e74fa6bd331679520153f4401b121298
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-18 22:33:53 +0100

    Updated the default test scenarios created by angular-cli

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UsersModule } from './users/users.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        HttpClientModule,
+        BrowserAnimationsModule,
+        UsersModule,
       ],
       declarations: [
         AppComponent
@@ -20,16 +26,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'santander'`, () => {
+  it(`should have as title 'Santander FE Test'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('santander');
+    expect(app.title).toEqual('Santander FE Test');
   });
 
-  it('should render title', () => {
+  it('should render header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('santander app is running!');
+    expect(compiled.querySelector('header > span').textContent).toContain('Santander Frontend Test');
   });
 });
