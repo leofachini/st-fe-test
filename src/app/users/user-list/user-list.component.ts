2e4ff120a0061c24f8647bf11d023c97d147533a
Leonardo F. Martins <leonardo.martins@navita.com.br>
2020-05-18 19:11:50 +0100

    Fetch the user list data

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { User } from '../user.model';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.scss'],
 })
-export class UserListComponent {
+export class UserListComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsersList()
+      .subscribe(users => this.users = users);
+  }
 
 }
